2bb7a17506db7e5d945c2006daaa425ac550aca8
Sungho Cho <sunghoch@andrew.cmu.edu>
2019-02-13 21:27:52 -0500

    Modified part1.py and exported txt file from dat

--- part_1_read_test_dat.py
+++ part_1_read_test_dat.py
@@ -5,3 +5,16 @@ input_dat_file = "data/pfgd_test.dat"
 
 #Use cc_dat_utils.make_cc_data_from_dat() to load the file specified by input_dat_file
 #print the resulting data
+
+
+
+if __name__ == '__main__':
+    # Reading from input dat file
+    dat_file = "data/pfgd_test.dat"
+    result = cc_dat_utils.make_cc_data_from_dat(dat_file)
+
+    # Writing spring representation to outfile
+    outfile = "data/pfgd_test.txt"
+    f = open(outfile,"w")
+    f.write(str(result))
+    f.close()
